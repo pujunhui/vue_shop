fd68c88792bb23849771c85786e27b97ff1e551e
pujh <pujh@idste.cn>
2020-06-18 23:02:34 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -9,9 +9,17 @@ import './assets/fonts/iconfont.css'
 
 import axios from 'axios'
 
-Vue.prototype.$http = axios
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+
+// axios请求拦截
+axios.interceptors.request.use(config => {
+  // 为请求头对象，添加Token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,27 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
+import Roles from '../components/rights/Roles.vue'
+import Rights from '../components/rights/Rights.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+      { path: '/roles', component: Roles },
+      { path: '/rights', component: Rights }
+    ]
+  }
 ]
 
 const router = new VueRouter({
